46308e25b61d1ca717b149a112a1fcaf0546d6ab
a49ibrahim <a49ibrahim@gmail.com>
2021-07-26 14:53:02 -0700

    set up fonts and basic ui for input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "./style.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faTimes } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function App() {
+  const handleInputChange = e => {
+    console.log(e.target.value)
+  }
+  const showHide = () => {
+    //
+  }
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <div className="contaner">
+        <input
+          type="password"
+          className="password"
+          placeholder="Enter your password"
+          onChange={handleInputChange}
+        />
+        <FontAwesomeIcon icon={faEye} id="show_hide" onClick={showHide}/>
+        <p id="capital"></p>
+      </div>
     </div>
   );
 }
