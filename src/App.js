1cf314cd9adcd52c39daa4fdbc30a1cd5699eb72
a49ibrahim <a49ibrahim@gmail.com>
2021-07-26 17:14:05 -0700

    add the password validations

--- src/App.js
+++ src/App.js
@@ -1,27 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { 
+  faEye, 
+  faTimes, 
+  faCheck,
+  faEyeSlash
+} from '@fortawesome/free-solid-svg-icons'
 
 
 export default function App() {
-  const handleInputChange = e => {
-    console.log(e.target.value)
+  const [show, setShow] = useState(false);
+  //if valid
+  const  valid = (item, v_icon, inv_icon) => {
+    let text = document.querySelector(`#${item}`);
+    text.style.opacity = "1";
+
+    let valid_icon = document.querySelector(`#${item} .${v_icon}`);
+    valid_icon.style.opacity = "1";
+
+    let invalid_icon = document.querySelector(`#${item} .${inv_icon}`);
+    invalid_icon.style.opacity = "0";
+  };
+
+  //if invalid
+  const invalid = (item, v_icon, inv_icon) => {
+    let text = document.querySelector(`#${item}`);
+    text.style.opacity = ".5";
+
+    let valid_icon = document.querySelector(`#${item} .${v_icon}`);
+    valid_icon.style.opacity = "0";
+
+    let invalid_icon = document.querySelector(`#${item} .${inv_icon}`);
+    invalid_icon.style.opacity = "1";
   }
-  const showHide = () => {
-    //
+
+  //handle input
+  const handleInputChange = e => {
+    const password = e.target.value;
+    if(password.match(/[A-Z]/) != null ){
+      valid("capital", "fa-check", "fa-times");
+    }else{
+      invalid("capital", "fa-check", "fa-times");
+    }
+    if(password.match(/[0-9]/) != null){
+      valid("num", "fa-check", "fa-times");
+    }else{
+      invalid("num", "fa-check", "fa-times");
+    }
+    if(password.match(/[!@#$%^&*]/) != null){
+      valid("char", "fa-check", "fa-times");
+    }else{
+      invalid("char", "fa-check", "fa-times");
+    }
+    if(password.length > 7){
+      valid("more8", "fa-check", "fa-times");
+    }else{
+      invalid("more8", "fa-check", "fa-times");
+    }
+  }
+  const handleShowhide = () => {
+    setShow(!show);
   }
   return (
     <div className="App">
-      <div className="contaner">
+      <div className="container">
         <input
-          type="password"
+          type={show ? "text" : "password" }
           className="password"
           placeholder="Enter your password"
           onChange={handleInputChange}
         />
-        <FontAwesomeIcon icon={faEye} id="show_hide" onClick={showHide}/>
-        <p id="capital"></p>
+        {
+          show ? (
+            <FontAwesomeIcon
+              onClick={handleShowhide} 
+              icon={faEye} 
+              id="show_hide" 
+            />
+          ) : (
+            <FontAwesomeIcon
+              onClick={handleShowhide} 
+              icon={faEyeSlash} 
+              id="show_hide" 
+            />
+          )
+        }
+        <p id="capital">
+          <FontAwesomeIcon className="fa-times icon" icon={faTimes} />
+          <FontAwesomeIcon className="fa-check icon" icon={faCheck} />
+          <span>capital Letters</span>
+        </p>
+        <p id="char">
+          <FontAwesomeIcon className="fa-times icon" icon={faTimes} />
+          <FontAwesomeIcon className="fa-check icon" icon={faCheck} />
+          <span>Special Characters</span>
+        </p>
+        <p id="num">
+          <FontAwesomeIcon className="fa-times icon" icon={faTimes} />
+          <FontAwesomeIcon className="fa-check icon" icon={faCheck} />
+          <span>Use Numbers</span>
+        </p>
+        <p id="more8">
+          <FontAwesomeIcon className="fa-times icon" icon={faTimes} />
+          <FontAwesomeIcon className="fa-check icon" icon={faCheck} />
+          <span>8 or more characters</span>
+        </p>
       </div>
     </div>
   );
